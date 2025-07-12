8bb05f534988c1b4053e0daacf1f4239ee615ac7
RicardoMurciaa <GM220529@alumno.udb.edu.sv>
2025-07-12 04:43:05 +0200

    Agregando semana2 con proyecto GM220529

---
+++ Semana2/GM220529/src/main/java/dwf/semana2/GM220529/Gm220529Application.java
@@ -0,0 +1,13 @@
+package dwf.semana2.GM220529;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gm220529Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gm220529Application.class, args);
+	}
+
+}
